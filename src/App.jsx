aa9cc79ff0e0315a1a6b2fd6b0543d07e6b9b19e
JS零基础 <13194175+js-zero-foundation@user.noreply.gitee.com>
2024-04-22 20:17:20 +0800

    “二次”

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React,{Component} from 'react'
+import React, { Component } from "react";
 import "./style.css";
 class App extends Component {
-  render(){
-    return <div className="box">Hello,Jack</div>;
+  render() {
+    return <div className="box">Hello,Jack1111</div>;
   }
 }
-export default App
+export default App;
